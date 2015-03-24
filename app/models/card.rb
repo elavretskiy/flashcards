362504b7dffbf0965663e76fa625a94e657419fe
lavrik <e.lavretskiy@gmail.com>
2015-03-25 02:15:20 +0300

    Добавлена недостающая валидация, исправлена функция downcase для кириллицы,

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,16 +1,21 @@
 class Card < ActiveRecord::Base
-  before_create :set_review
-  validate :check_card
+  before_validation :set_review
+  validate :check_original_translated_equal
+  validates :original, presence: true
+  validates :translated, presence: true
+  validates :review, presence: true
 
   protected
     def set_review
       self.review = Time.now + 3.days
     end
 
-    def check_card
-      self.original = original.downcase
-      self.translated = translated.downcase
-      errors.add(:original, ' = Translated. Вводимые значения должны отличаться.') if
-          original == translated
+    def check_original_translated_equal
+      self.original = original.mb_chars.downcase.to_s.squeeze(' ').lstrip
+      self.translated = translated.mb_chars.downcase.to_s.squeeze(' ').lstrip
+      if original == translated
+        errors.add(:original,
+                   ' = Translated. Вводимые значения должны отличаться.')
+      end
     end
 end
