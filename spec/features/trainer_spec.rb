90796344c91c5587c4c01c811cb65964a793dd33
lavrik <e.lavretskiy@gmail.com>
2015-03-27 16:31:19 +0300

    Добавлен общий describe для trainer_spec.

--- spec/features/trainer_spec.rb
+++ spec/features/trainer_spec.rb
@@ -1,63 +1,65 @@
 require 'rails_helper'
 
-describe "training without cards" do
-  before :each do
-    create(:card)
-  end
+describe "review cards" do
+  describe "training without cards" do
+    before :each do
+      create(:card)
+    end
 
-  it 'no cards' do
-    visit root_path
-    expect(page).to have_content 'Ожидайте наступления даты пересмотра.'
-  end
-end
-
-describe "training with some cards" do
-  before :each do
-    (0..1).each do
-      card = create(:card)
-      card.update_attribute(:review_date, Time.now)
+    it 'no cards' do
+      visit root_path
+      expect(page).to have_content 'Ожидайте наступления даты пересмотра.'
     end
   end
 
-  it 'first visit' do
-    visit root_path
-    expect(page).to have_content 'Original text'
+  describe "training with some cards" do
+    before :each do
+      (0..1).each do
+        card = create(:card)
+        card.update_attribute(:review_date, Time.now)
+      end
+    end
+
+    it 'first visit' do
+      visit root_path
+      expect(page).to have_content 'Original text'
+    end
+
+    it 'incorrect translation' do
+      visit root_path
+      fill_in 'user_translation', with: 'RoR'
+      click_button 'Проверить'
+      expect(page).
+          to have_content 'Вы ввели не верный перевод. Повторите попытку.'
+    end
+
+    it 'correct translation' do
+      visit root_path
+      fill_in 'user_translation', with: 'house'
+      click_button 'Проверить'
+      expect(page).to have_content 'Вы ввели верный перевод. Продолжайте.'
+    end
   end
 
-  it 'incorrect translation' do
-    visit root_path
-    fill_in 'user_translation', with: 'RoR'
-    click_button 'Проверить'
-    expect(page).
-        to have_content 'Вы ввели не верный перевод. Повторите попытку.'
-  end
+  describe "training with one card" do
+    before :each do
+      card = create(:card)
+      card.update_attribute(:review_date, Time.now)
+    end
 
-  it 'correct translation' do
-    visit root_path
-    fill_in 'user_translation', with: 'house'
-    click_button 'Проверить'
-    expect(page).to have_content 'Вы ввели верный перевод. Продолжайте.'
-  end
-end
+    it 'incorrect translation' do
+      visit root_path
+      fill_in 'user_translation', with: 'RoR'
+      click_button 'Проверить'
+      expect(page).
+          to have_content 'Вы ввели не верный перевод. Повторите попытку.'
+    end
 
-describe "training with one card" do
-  before :each do
-    card = create(:card)
-    card.update_attribute(:review_date, Time.now)
-  end
-
-  it 'incorrect translation' do
-    visit root_path
-    fill_in 'user_translation', with: 'RoR'
-    click_button 'Проверить'
-    expect(page).
-        to have_content 'Вы ввели не верный перевод. Повторите попытку.'
-  end
-
-  it 'correct translation' do
-    visit root_path
-    fill_in 'user_translation', with: 'house'
-    click_button 'Проверить'
-    expect(page).to have_content 'Ожидайте наступления даты пересмотра.'
+    it 'correct translation' do
+      visit root_path
+      fill_in 'user_translation', with: 'house'
+      click_button 'Проверить'
+      expect(page).to have_content 'Ожидайте наступления даты пересмотра.'
+    end
   end
 end
