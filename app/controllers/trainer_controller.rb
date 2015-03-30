06e277a57b780bb9ff332a7debe076e38a396bff
lavrik <e.lavretskiy@gmail.com>
2015-03-30 13:15:45 +0300

    Removed profile edit spec

--- app/controllers/trainer_controller.rb
+++ app/controllers/trainer_controller.rb
@@ -2,7 +2,7 @@ class TrainerController < ApplicationController
   respond_to :html
 
   def review
-    @card = current_user.cards.find(params[:id])
+    @card = current_user.cards.find(params[:card_id])
     if @card.check_translation(trainer_params[:user_translation])
       flash[:notice] = 'Вы ввели верный перевод. Продолжайте.'
       redirect_to root_path
