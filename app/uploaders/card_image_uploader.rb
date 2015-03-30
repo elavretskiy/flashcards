50d41b4f747cd133a511d0903d888fc231df3a30
lavrik <e.lavretskiy@gmail.com>
2015-03-30 21:06:59 +0300

    Привел card_image_uploder в надлежащий вид, по хаунду.

--- app/uploaders/card_image_uploader.rb
+++ app/uploaders/card_image_uploader.rb
@@ -7,7 +7,7 @@ class CardImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}"
   end
 
-  process resize_to_fit: [360,360]
+  process resize_to_fit: [360, 360]
   process convert: 'jpg'
 
   def extension_white_list
@@ -17,5 +17,4 @@ class CardImageUploader < CarrierWave::Uploader::Base
   def filename
     "#{model.id}.#{file.extension.downcase}" if original_filename
   end
-
 end
