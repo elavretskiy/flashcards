102e507fdedef1dcb574809fd5e7e8ad9fd55542
lavrik <e.lavretskiy@gmail.com>
2015-04-02 20:07:12 +0300

    Исправлен review_card по хаунду

--- app/controllers/trainer_controller.rb
+++ app/controllers/trainer_controller.rb
@@ -4,23 +4,22 @@ class TrainerController < ApplicationController
   def review_card
     @card = current_user.cards.find(params[:card_id])
 
-    levenshtein_distance =
-        @card.check_translation(trainer_params[:user_translation])
+    levenshtein_distance = @card.check_translation(trainer_params[:user_translation])
 
     case levenshtein_distance
-      when 0
-        flash[:notice] = 'Вы ввели верный перевод. Продолжайте.'
-        redirect_to root_path
-      when 1
-        flash[:alert] =
-            "Вы ввели перевод c опечаткой.
-          Ваш вариант: #{trainer_params[:user_translation]} /
-          Оригинал: #{@card.original_text} /
-          Перевод: #{@card.translated_text}. Продолжайте."
-        redirect_to root_path
-      else
-        flash[:alert] = 'Вы ввели не верный перевод. Повторите попытку.'
-        redirect_to root_path(id: @card.id)
+    when 0
+      flash[:notice] = 'Вы ввели верный перевод. Продолжайте.'
+      redirect_to root_path
+    when 1
+      flash[:alert] =
+        "Вы ввели перевод c опечаткой.
+        Ваш вариант: #{trainer_params[:user_translation]} /
+        Оригинал: #{@card.original_text} /
+        Перевод: #{@card.translated_text}. Продолжайте."
+      redirect_to root_path
+    else
+      flash[:alert] = 'Вы ввели не верный перевод. Повторите попытку.'
+      redirect_to root_path(id: @card.id)
     end
   end
 
