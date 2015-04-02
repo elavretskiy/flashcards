94f8bba11cb31e059deb47f4092183fd0334496e
lavrik <e.lavretskiy@gmail.com>
2015-04-02 19:03:14 +0300

    Задание 14: Делаем жизнь пользователя проще

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -77,49 +77,49 @@ describe Card do
   it 'check_translation Eng OK' do
     card = Card.create(original_text: 'дом', translated_text: 'house',
                        user_id: 1, block_id: 1)
-    expect(card.check_translation('house')).to be true
+    expect(card.check_translation('house')).to be 0
   end
 
   it 'check_translation Eng NOT' do
     card = Card.create(original_text: 'дом', translated_text: 'house',
                        user_id: 1, block_id: 1)
-    expect(card.check_translation('RoR')).to be false
+    expect(card.check_translation('RoR')).to be nil
   end
 
   it 'check_translation Rus OK' do
     card = Card.create(original_text: 'house', translated_text: 'дом',
                        user_id: 1, block_id: 1)
-    expect(card.check_translation('дом')).to be true
+    expect(card.check_translation('дом')).to be 0
   end
 
   it 'check_translation Rus NOT' do
     card = Card.create(original_text: 'house', translated_text: 'дом',
                        user_id: 1, block_id: 1)
-    expect(card.check_translation('RoR')).to be false
+    expect(card.check_translation('RoR')).to be nil
   end
 
   it 'check_translation full_downcase Eng OK' do
     card = Card.create(original_text: 'ДоМ', translated_text: 'hOuSe',
                        user_id: 1, block_id: 1)
-    expect(card.check_translation('HousE')).to be true
+    expect(card.check_translation('HousE')).to be 0
   end
 
   it 'check_translation full_downcase Eng NOT' do
     card = Card.create(original_text: 'ДоМ', translated_text: 'hOuSe',
                        user_id: 1, block_id: 1)
-    expect(card.check_translation('RoR')).to be false
+    expect(card.check_translation('RoR')).to be nil
   end
 
   it 'check_translation full_downcase Rus OK' do
     card = Card.create(original_text: 'hOuSe', translated_text: 'ДоМ',
                        user_id: 1, block_id: 1)
-    expect(card.check_translation('дОм')).to be true
+    expect(card.check_translation('дОм')).to be 0
   end
 
   it 'check_translation full_downcase Rus NOT' do
     card = Card.create(original_text: 'hOuSe', translated_text: 'ДоМ',
                        user_id: 1, block_id: 1)
-    expect(card.check_translation('RoR')).to be false
+    expect(card.check_translation('RoR')).to be nil
   end
 
   it 'create card witout user_id' do
@@ -134,5 +134,29 @@ describe Card do
                        user_id: 1)
     expect(card.errors[:block_id]).
         to include('Выберите колоду из выпадающего списка.')
+  end
+
+  it 'check_translation Eng OK levenshtein_distance=1' do
+    card = Card.create(original_text: 'дом', translated_text: 'hous',
+                       user_id: 1, block_id: 1)
+    expect(card.check_translation('house')).to be 1
+  end
+
+  it 'check_translation Rus OK levenshtein_distance=1' do
+    card = Card.create(original_text: 'house', translated_text: 'до',
+                       user_id: 1, block_id: 1)
+    expect(card.check_translation('дом')).to be 1
+  end
+
+  it 'check_translation Eng NOT levenshtein_distance=2' do
+    card = Card.create(original_text: 'дом', translated_text: 'hou',
+                       user_id: 1, block_id: 1)
+    expect(card.check_translation('RoR')).to be nil
+  end
+
+  it 'check_translation Rus NOT levenshtein_distance=2' do
+    card = Card.create(original_text: 'house', translated_text: 'д',
+                       user_id: 1, block_id: 1)
+    expect(card.check_translation('RoR')).to be nil
   end
 end
