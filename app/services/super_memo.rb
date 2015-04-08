b66d57297a0cf75c75c8f9b40afef78c6222d00a
lavrik <e.lavretskiy@gmail.com>
2015-04-08 14:49:26 +0300

    Применил глобальный метод

--- app/services/super_memo.rb
+++ app/services/super_memo.rb
@@ -3,44 +3,56 @@
 # http://www.supermemo.com/english/ol/sm2.htm
 
 class SuperMemo
-  def self.algorithm(interval, repeat, efactor, attempt, distance, distance_limit)
-    quality = set_quality(attempt, distance, distance_limit)
-    efactor = set_efactor(efactor, quality)
-    sm_hash = if quality >= 3
-                set_interval(interval, repeat + 1, efactor)
-              else
-                set_interval(interval, 1, efactor)
-              end
-    sm_hash.merge!(quality: quality)
-  end
+  class << self
+    def [](interval, repeat, efactor, attempt, distance, distance_limit)
+      algorithm(interval, repeat, efactor, attempt, distance, distance_limit)
+    end
 
-  def self.set_interval(interval, repeat, efactor)
-    interval = case repeat
-               when 1 then 1
-               when 2 then 6
-               else (interval * efactor).round
-               end
-    { interval: interval, efactor: efactor, repeat: repeat }
-  end
+    def algorithm(interval, repeat, efactor, attempt, distance, distance_limit)
+      quality = set_quality(attempt, distance, distance_limit)
+      efactor = set_efactor(efactor, quality)
+      sm_hash = if quality >= 3
+                  set_interval(interval, repeat + 1, efactor)
+                else
+                  set_interval(interval, 1, efactor)
+                end
+      sm_hash.merge!(quality: quality)
+    end
 
-  def self.set_efactor(efactor, quality)
-    efactor = efactor + (0.1 - (5 - quality) * (0.08 + (5 - quality) * 0.02))
-    efactor < 1.3 ? 1.3 : efactor
-  end
+    def set_interval(interval, repeat, efactor)
+      interval = case repeat
+                 when 1 then 1
+                 when 2 then 6
+                 else (interval * efactor).round
+                 end
+      { interval: interval, efactor: efactor, repeat: repeat }
+    end
 
-  def self.set_quality(attempt, distance, distance_limit)
-    if distance <= distance_limit
-      case attempt
-      when 1 then 5
-      when 2 then 4
-      else 3
-      end
-    else
-      case attempt
-      when 1 then 2
-      when 2 then 1
-      else 0
+    def set_efactor(efactor, quality)
+      efactor = efactor + (0.1 - (5 - quality) * (0.08 + (5 - quality) * 0.02))
+      efactor < 1.3 ? 1.3 : efactor
+    end
+
+    def set_quality(attempt, distance, distance_limit)
+      if distance <= distance_limit
+        case attempt
+        when 1 then 5
+        when 2 then 4
+        else 3
+        end
+      else
+        case attempt
+        when 1 then 2
+        when 2 then 1
+        else 0
+        end
       end
     end
+  end
+end
+
+module Kernel
+  def SuperMemo(interval, repeat, efactor, attempt, distance, distance_limit)
+    SuperMemo.algorithm(interval, repeat, efactor, attempt, distance, distance_limit)
   end
 end
