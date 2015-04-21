7720bed1c15dab2eb3bf834d71c38dca22290a9d
lavrik <e.lavretskiy@gmail.com>
2015-04-21 11:55:51 +0300

    Изменил настройки тестовой среды, для запуска тестов на travis

--- app/mailers/friends_mailer.rb
+++ app/mailers/friends_mailer.rb
@@ -1,6 +1,11 @@
 class FriendsMailer < ActionMailer::Base
   layout 'mailers'
-  default from: ENV['DEFAULT_EMAIL_FROM_CARDS']
+
+  unless Rails.env.test? || Rails.env.cucumber?
+    default from: ENV['DEFAULT_EMAIL_FROM_CARDS']
+  else
+    default from: 'info@flashcards.com'
+  end
 
   def invite(email)
     mail(to: email, subject: 'Приглашаем Вас на сайт flashcards.')
