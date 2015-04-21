6f65060b08d131d09d17f5343f5ef4a7f9e7aa09
lavrik <e.lavretskiy@gmail.com>
2015-04-21 13:40:02 +0300

    Упростил алгоритм приглашения друзей

--- app/services/friends_service.rb
+++ app/services/friends_service.rb
@@ -1,17 +1,17 @@
 class FriendsService
   class << self
     def invite(emails)
-      @incorrect_emails = ''
+      @incorrect_emails = []
       emails = emails.select do |email|
         if email[/\A([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})\z/i]
-          User.exists?(email: email) ? false : true
+          !User.exists?(email: email)
         else
-          @incorrect_emails += @incorrect_emails.blank? ? email : (', ' + email)
+          @incorrect_emails << email
         end
       end
 
-      emails.each { |email| FriendsMailer.invite(email).deliver_now } if @incorrect_emails.blank?
-      @incorrect_emails
+      emails.each { |email| FriendsMailer.invite(email).deliver_now } if @incorrect_emails.empty?
+      @incorrect_emails.join(', ')
     end
   end
 end
