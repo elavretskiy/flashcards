59f2b061d9b95c57eec866b6617af6c8a03d42ba
Evgeniy Lavretskiy <e.lavretskiy@gmail.com>
2015-06-09 18:56:55 +0300

    Написаны тесты для экшена parsing_html?

---
+++ spec/controllers/cards_controller_spec.rb
@@ -0,0 +1,85 @@
+require 'rails_helper'
+
+describe Dashboard::CardsController do
+  describe 'parsing_html' do
+    before do
+      @user = create(:user)
+      @block = create(:block, user: @user)
+      @controller.send(:auto_login, @user)
+    end
+
+    it 'add delayed job' do
+      post :parsing_html, { format: 'js',
+                            parsing_html:
+                              { block_id: @block.id,
+                                url: 'http://www.learnathome.ru/blog/100-beautiful-words',
+                                css_original: 'table tbody tr td[2] p',
+                                css_translated: 'table tbody tr td[1] p' }}
+      expect(Delayed::Job.count).to eq(1)
+      expect(flash[:notice]).to eq('Задача на парсинг сайта успешно поставлена в очередь.')
+      expect(flash[:alert]).to eq(nil)
+    end
+
+    it 'incorrect url' do
+      post :parsing_html, { format: 'js',
+                            parsing_html:
+                              { block_id: @block.id,
+                                url: 'http://www.learnathome.ru/blog/100-beautiful',
+                                css_original: 'table tbody tr td[2] p',
+                                css_translated: 'table tbody tr td[1] p' }}
+      expect(Delayed::Job.count).to eq(0)
+      expect(flash[:alert]).to eq('Проверьте правильность введенных данных.')
+      expect(flash[:notice]).to eq(nil)
+    end
+
+    it 'incorrect css original' do
+      post :parsing_html, { format: 'js',
+                            parsing_html:
+                              { block_id: @block.id,
+                                url: 'http://www.learnathome.ru/blog/100-beautiful-words',
+                                css_original: 'table tbody',
+                                css_translated: 'table tbody tr td[1] p' }}
+      expect(Delayed::Job.count).to eq(0)
+      expect(flash[:alert]).to eq('Проверьте правильность введенных данных.')
+      expect(flash[:notice]).to eq(nil)
+    end
+
+    it 'incorrect css translated' do
+      post :parsing_html, { format: 'js',
+                            parsing_html:
+                              { block_id: @block.id,
+                                url: 'http://www.learnathome.ru/blog/100-beautiful-words',
+                                css_original: 'table tbody tr td[2] p',
+                                css_translated: 'table tbody' }}
+      expect(Delayed::Job.count).to eq(0)
+      expect(flash[:alert]).to eq('Проверьте правильность введенных данных.')
+      expect(flash[:notice]).to eq(nil)
+    end
+
+    it 'empty block field' do
+      post :parsing_html, { format: 'js',
+                            parsing_html:
+                              { url: 'http://www.learnathome.ru/blog/100-beautiful-words',
+                                css_original: 'table tbody tr td[2] p',
+                                css_translated: 'table tbody' }}
+      expect(Delayed::Job.count).to eq(0)
+      expect(flash[:alert]).to eq('Проверьте правильность введенных данных.')
+      expect(flash[:notice]).to eq(nil)
+    end
+
+    it 'create cards from html' do
+      Delayed::Worker.delay_jobs = false
+      post :parsing_html, { format: 'js',
+                            parsing_html:
+                              { block_id: @block.id,
+                                url: 'http://www.learnathome.ru/blog/100-beautiful-words',
+                                css_original: 'table tbody tr td[2] p',
+                                css_translated: 'table tbody tr td[1] p' }}
+      expect(Delayed::Job.count).to eq(0)
+      expect(flash[:notice]).to eq('Задача на парсинг сайта успешно поставлена в очередь.')
+      expect(flash[:alert]).to eq(nil)
+      expect(Card.exists?(original_text: 'вода')).to eq(true)
+      expect(Card.exists?(translated_text: 'aqua')).to eq(true)
+    end
+  end
+end
