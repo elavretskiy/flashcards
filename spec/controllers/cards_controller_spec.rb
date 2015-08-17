63c0c45927d8de91c5075043c5acae7ad8ecf16f
Evgeniy Lavretskiy <e.lavretskiy@gmail.com>
2015-08-17 17:17:42 +0300

    Fix hound

--- spec/controllers/cards_controller_spec.rb
+++ spec/controllers/cards_controller_spec.rb
@@ -9,59 +9,74 @@ describe Dashboard::CardsController do
     end
 
     it "add delayed job" do
-      post :parsing_html, { format: "js",
-                            parsing_html:
-                              { block_id: @block.id,
-                                url: "http://www.learnathome.ru/blog/100-beautiful-words",
-                                origin_selector: "table tbody tr td[2] p",
-                                translated_selector: "table tbody tr td[1] p" }}
+      post :parsing_html,
+           format: "js",
+           parsing_html:
+             {
+               block_id: @block.id,
+               url: "http://www.learnathome.ru/blog/100-beautiful-words",
+               origin_selector: "table tbody tr td[2] p",
+               translated_selector: "table tbody tr td[1] p"
+             }
       expect(Delayed::Job.count).to eq(1)
       expect(flash[:notice]).to eq("Задача на парсинг сайта успешно поставлена в очередь.")
       expect(flash[:alert]).to eq(nil)
     end
 
     it "incorrect url" do
-      post :parsing_html, { format: "js",
-                            parsing_html:
-                              { block_id: @block.id,
-                                url: "http://www.learnathome.ru/blog/100-beautiful",
-                                origin_selector: "table tbody tr td[2] p",
-                                translated_selector: "table tbody tr td[1] p" }}
+      post :parsing_html,
+           format: "js",
+           parsing_html:
+             {
+               block_id: @block.id,
+               url: "http://www.learnathome.ru/blog/100-beautiful",
+               origin_selector: "table tbody tr td[2] p",
+               translated_selector: "table tbody tr td[1] p"
+             }
       expect(Delayed::Job.count).to eq(0)
       expect(flash[:alert]).to eq("Проверьте правильность введенных данных.")
       expect(flash[:notice]).to eq(nil)
     end
 
     it "incorrect css original" do
-      post :parsing_html, { format: "js",
-                            parsing_html:
-                              { block_id: @block.id,
-                                url: "http://www.learnathome.ru/blog/100-beautiful-words",
-                                origin_selector: "table tbody",
-                                translated_selector: "table tbody tr td[1] p" }}
+      post :parsing_html,
+           format: "js",
+           parsing_html:
+             {
+               block_id: @block.id,
+               url: "http://www.learnathome.ru/blog/100-beautiful-words",
+               origin_selector: "table tbody",
+               translated_selector: "table tbody tr td[1] p"
+             }
       expect(Delayed::Job.count).to eq(0)
       expect(flash[:alert]).to eq("Проверьте правильность введенных данных.")
       expect(flash[:notice]).to eq(nil)
     end
 
     it "incorrect css translated" do
-      post :parsing_html, { format: "js",
-                            parsing_html:
-                              { block_id: @block.id,
-                                url: "http://www.learnathome.ru/blog/100-beautiful-words",
-                                origin_selector: "table tbody tr td[2] p",
-                                translated_selector: "table tbody" }}
+      post :parsing_html,
+           format: "js",
+           parsing_html:
+             {
+               block_id: @block.id,
+               url: "http://www.learnathome.ru/blog/100-beautiful-words",
+               origin_selector: "table tbody tr td[2] p",
+               translated_selector: "table tbody"
+             }
       expect(Delayed::Job.count).to eq(0)
       expect(flash[:alert]).to eq("Проверьте правильность введенных данных.")
       expect(flash[:notice]).to eq(nil)
     end
 
     it "empty block field" do
-      post :parsing_html, { format: "js",
-                            parsing_html:
-                              { url: "http://www.learnathome.ru/blog/100-beautiful-words",
-                                origin_selector: "table tbody tr td[2] p",
-                                translated_selector: "table tbody" }}
+      post :parsing_html,
+           format: "js",
+           parsing_html:
+             {
+               url: "http://www.learnathome.ru/blog/100-beautiful-words",
+               origin_selector: "table tbody tr td[2] p",
+               translated_selector: "table tbody"
+             }
       expect(Delayed::Job.count).to eq(0)
       expect(flash[:alert]).to eq("Проверьте правильность введенных данных.")
       expect(flash[:notice]).to eq(nil)
@@ -69,12 +84,15 @@ describe Dashboard::CardsController do
 
     it "create cards from html" do
       Delayed::Worker.delay_jobs = false
-      post :parsing_html, { format: "js",
-                            parsing_html:
-                              { block_id: @block.id,
-                                url: "http://www.learnathome.ru/blog/100-beautiful-words",
-                                origin_selector: "table tbody tr td[2] p",
-                                translated_selector: "table tbody tr td[1] p" }}
+      post :parsing_html,
+           format: "js",
+           parsing_html:
+             {
+               block_id: @block.id,
+               url: "http://www.learnathome.ru/blog/100-beautiful-words",
+               origin_selector: "table tbody tr td[2] p",
+               translated_selector: "table tbody tr td[1] p"
+             }
       expect(Delayed::Job.count).to eq(0)
       expect(flash[:notice]).to eq("Задача на парсинг сайта успешно поставлена в очередь.")
       expect(flash[:alert]).to eq(nil)
